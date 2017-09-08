ca23f2bbb9c99be1bd2e2e42f8697d7d43056568
brsoyan <haykbrsoyan@gmail.com>
2017-09-08 12:37:08 +0400

    Add config for categories

--- README.md
+++ README.md
@@ -5,5 +5,19 @@ Set your file path
 project_path = "/Users/hb/Desktop/picsart-ios"
 
 You can set file prefix which you need to find.
+```
 prefix = "HB"
+```
 
+If you need to remove unused categories imports chanage this BOOL value to True
+```
+remove_categories = False
+```
+# If in your project you have classes which #imports you doesn't need to remove.
+# Or classes for static object
+# Like this static NSString *CELL_IDENTIFIER = @"mainCell"; ....
+# And does not need to delete import add the classes names in constant_classes list.
+
+```
+constant_classes = ["HBConstantString", "HBApiCallUrls"]
+```
--- unused_imports.py
+++ unused_imports.py
@@ -1,13 +1,24 @@
 # This code for python 3
 
-import os, glob, mmap, re, fileinput
-
+import os, glob, re
+from collections import Counter
 
 # your project path
-project_path = "/Users/user/Desktop/project_PATH"
+project_path = "/Users/user/Desktop/your-Project"
 files = [];
 # you can set file prefix which you need to find
-prefix = "HB"
+prefix = "SC"
+
+#if you need to remove unused categories imports chanage this BOOL
+remove_categories = False
+
+# If in your project you have classes which #imports you doesn't need to remove.
+# Or classes for static object
+# Like this static NSString *CELL_IDENTIFIER = @"mainCell"; ....
+# And does not need to delete import add the classes names in constant_classes list.
+
+constant_classes = ["HBConstantString", "HBApiCallUrls"] # ....
+
 
 def find_files_recursive(path):
 	for filename in os.listdir(path):
@@ -19,9 +30,9 @@ def find_files_recursive(path):
 			words = filename.split("/")
 			last_Word = words[-1]
 			if len(prefix) > 0:
-				if last_Word.startswith(prefix):
+				if last_Word.startswith(prefix) and filename not in files:
 					files.append(filename)
-			else:
+			elif filename not in files:
 				files.append(filename)
 
 find_files_recursive(project_path)
@@ -38,18 +49,35 @@ for file in files:
 			if line.startswith("#import"):
 				words = line.split()
 				word = re.sub('["]', '', words[1].split(".")[0])
-				if word not in class_names:
+				if constant_classes.count(word) > 0 and word not in class_names:
 					class_names.append(word)
+					f.write(line)
+				elif word.startswith(prefix) or word.startswith("NS"):
+					if not remove_categories:
+						if "+" in word and word not in class_names:
+							class_names.append(word)
+							f.write(line)
+						else:
+							count = 0
+							for x_word in lines:
+								if x_word.count(word) > 0:
+									count += 1
+							if word not in class_names and count != 1:
+								class_names.append(word)
+								f.write(line)
+					else:
+						count = 0
+						for x_word in lines:
+							if x_word.count(word) > 0:
+								count += 1
+
+						if word not in class_names and count != 1:
+							class_names.append(word)
+							f.write(line)
+				else:
 					f.write(line)
 			else:
 				f.write(line)
-
 		f.close()
-
-	print(class_names)
-
-
-
-
 
 
